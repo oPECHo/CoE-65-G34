2ae31d66121b9ecd42ba40752e8ba94b906cb2c2
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-02-04 12:16:58 +0700

    แก้ไข code เพิ่อให้ -1 กระจายทั่วถึง

--- mine/main.py
+++ mine/main.py
@@ -6,10 +6,10 @@ def random_bom(rows: int, cols: int, bom_number: int) -> list:
     mine = []
 
     for row in range(rows):
-        row_data = []
+        row_data = [] 
         for col in range(cols):
-            position = random.randint(0, 1)
-            if position == 1 and bom_number > 0:
+            position = random.randint(0, 5)
+            if position == 5 and bom_number > 0:
                 row_data.append(-1)
                 bom_number -= 1
             else:
